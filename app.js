753d04621a91ae3325a4e8cc3a7160501e0645df
Sergey <sergey.lapshin.00@mail.ru>
2022-05-24 21:12:03 +0700

    add search bar

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelector('#input').oninput = function(){
+    let val = this.value.trim();
+    let inputItems = document.querySelectorAll('.auditories li');
+    if (val != ''){
+        inputItems.forEach(function(elem){
+            if (elem.innerText.search(val) == -1) {
+                elem.classList.add('hide');
+            }
+            else {
+                elem.classList.remove('hide')
+            }
+        })
+    }
+    else {
+        inputItems.forEach(function(elem) {
+            elem.classList.remove('hide');
+        });
+    }
+}
